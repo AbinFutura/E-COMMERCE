3ffe74fb6f2e8f2043648c19eaf6a8717b00d996
mirshab001 <mirshabpm01@gmail.com>
2025-04-11 11:10:31 +0530

    on boarding added

--- lib/utils/constants/images.dart
+++ lib/utils/constants/images.dart
@@ -1,10 +1,13 @@
 class EImages {
 
   // splash images
-  static  String splash = "assets/images/splash/splashhhh.png";
+  static  String splash1 = "assets/images/splash/splash screen.png";
 
   // onboarding images
-  static  String onboard1 = "assets/images/onboarding/on1-removebg-preview.png";
+  static String onboard1 = "assets/images/onboarding/Onboarding 1 (1).png";
+  static String onboard2= "assets/images/onboarding/Onboarding 2.png";
+  static String onboard3= "assets/images/onboarding/Onboarding 3.png";
+
 
 
 
