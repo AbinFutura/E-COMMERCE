98e1782690077f6fdbe67fa89634371c184ac9fd
mirshab001 <mirshabpm01@gmail.com>
2025-04-12 15:05:31 +0530

    cart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_project/views/cart/cart_page.dart';
 import 'package:e_commerce_project/views/onboard/onboard.dart';
+import 'package:e_commerce_project/views/profile/profile_page.dart';
 import 'package:e_commerce_project/views/splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
